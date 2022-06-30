235475227d2f8bf3f068ff58e410caa8523fc5cd
mattianaut23 <61780929+mattianaut23@users.noreply.github.com>
2022-06-30 10:49:42 +0200

    Update VectorTest1.java

--- examples/VectorTest1.java
+++ examples/VectorTest1.java
@@ -1,1 +1,35 @@
+import jdk.incubator.vector.*;
 
+public class VectorTest1 {
+    
+    static final VectorSpecies<Float> SPECIES = FloatVector.SPECIES_256;
+
+    static void vectorComputation(float[] a, float[] b, float[] c) {
+        for (int i = 0; i < a.length; i += SPECIES.length()) {
+            var m = SPECIES.indexInRange(i, a.length);
+            // FloatVector va, vb, vc;
+            //var va = FloatVector.fromArray(SPECIES, a, i, m);
+            //var vb = FloatVector.fromArray(SPECIES, b, i, m);
+            //var vc = va.mul(va).
+            //            add(vb.mul(vb)).
+                        neg();
+            //vc.intoArray(c, i, m);
+          System.out.println("Test");
+          System.out.println(m);
+        }
+}
+    
+    /*public static void main(String[] args) throws Exception {
+        float  a[] = {1,1,1,1,1,1,1,1};
+        float  b[] = {1,1,1,1,1,1,1,1};
+        float[] x = new float[8];
+
+        vectorComputation(a,b,x);
+        for(float e : x)
+            System.out.println(e);
+
+    }*/
+
+    
+
+}
