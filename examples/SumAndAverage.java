82e5e2a73e367442862a31174c8108003f941eb0
mattiacernelli <61780929+mattianaut23@users.noreply.github.com>
2022-07-12 12:14:58 +0200

    Update SumAndAverage.java

--- examples/SumAndAverage.java
+++ examples/SumAndAverage.java
@@ -2,9 +2,9 @@ import jdk.incubator.vector.*;
 
 public class SumAndAverage {
 
-      static final VectorSpecies<Integer> SPECIES = IntVector.SPECIES_256;
+      static final VectorSpecies<Float> SPECIES = FloatVector.SPECIES_256;
       
-      static void SumAndAverageComputation(int[] a) {
+      static void SumAndAverageComputation(float[] a) {
       
             //int sum = 0;
             for (int i=0; i < SPECIES.loopBound(a.length); i += SPECIES.length()) {       
@@ -17,7 +17,7 @@ public class SumAndAverage {
       
       public static void main(String[] args) throws Exception {
       
-      int a[] = {1,2,3,4,5,6,7,8,9,10};
+      float a[] = {1,2,3,4,5,6,7,8,9,10};
       SumAndAverageComputation(a);
       
       }
