8cd63a03e69eb62de307e23d6994effb9377edec
mattiacernelli <61780929+mattianaut23@users.noreply.github.com>
2022-07-14 19:21:24 +0200

    Update SumAndAverage.java

--- examples/SumAndAverage.java
+++ examples/SumAndAverage.java
@@ -23,7 +23,7 @@ public class SumAndAverage {
       
       public static void main(String[] args) throws Exception {
       
-      float a[] = {1,2,3,4,5,6,7,8,9,10};
+      float a[] = {1};
       SumAndAverageComputation(a);
       
       }
