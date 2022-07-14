5615ca936344daf45d20fbbdfb8ad8537f5023a2
mattiacernelli <61780929+mattianaut23@users.noreply.github.com>
2022-07-14 19:14:20 +0200

    Update SumAndAverage.java

--- examples/SumAndAverage.java
+++ examples/SumAndAverage.java
@@ -8,13 +8,15 @@ public class SumAndAverage {
       
             int sum = 0;
             float b[] = {0,0,0,0,0,0,0,0,0,0};
+            var i = SPECIES.loopBound(a.length);
+            System.out.println(i);   
           
-            for (int i=0; i < SPECIES.loopBound(a.length); i += SPECIES.length()) {    
+          /*  for (int i=0; i < SPECIES.loopBound(a.length); i += SPECIES.length()) {    
               FloatVector va = FloatVector.fromArray(SPECIES, a, i);
               System.out.println(va);                                   // 1 2 3 4 5 6 7 8
               va.intoArray(b,i);                 
               System.out.println(b);                                   // 2 3 4 5 6 7 8 9
-            }
+            }*/
             
       }
       
