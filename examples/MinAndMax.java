42f56b43b2f17d0bd8829a3a5f6bd5eb4645714f
mattiacernelli <61780929+mattianaut23@users.noreply.github.com>
2022-07-16 12:38:23 +0200

    Update and rename SumAndAverage.java to MinAndMax.java

---
+++ examples/MinAndMax.java
@@ -0,0 +1,27 @@
+import jdk.incubator.vector.*;
+
+public class MinAndMax {
+
+      static final VectorSpecies<Float> SPECIES = FloatVector.SPECIES_256;
+      
+      static void MinAndMaxComputation(float[] a) {
+                  
+             for (int i = 0; i < SPECIES.loopBound(a.length); i += SPECIES.length()) {
+                   FloatVector va = FloatVector.fromArray(SPECIES, a, i);                 
+                   FloatVector vb = va.min(3.0);
+                   System.out.println(vb);
+             }
+           
+      }
+      
+      
+      public static void main(String[] args) throws Exception {
+      
+      float a[] = {1,2};
+      MinAndMaxComputation(a);
+      
+      }
+      
+}      
+            
+                    
